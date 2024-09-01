88078e6ab6161dd16f41a72bdedf8a7f503f5b77
cuong tran <you@example.com>
2024-09-01 17:26:38 +0700

    push

---
+++ src/NumberExercise.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static java.util.Collections.sort;
+
+public class NumberExercise {
+    static List<Integer> numbers = new ArrayList<>();
+
+    public static void importData() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhập số lượng phần tử");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println("nhập phần tử thứ " + (i + 1));
+            int number = sc.nextInt();
+            numbers.add(number);
+        }
+    }
+
+    public void prinData() {
+        System.out.println(numbers);
+    }
+
+    public void findMax2() {
+        sort(numbers);
+        System.out.println("phần tử lớn thứ 2 là " + numbers.get(numbers.size() - 2));
+    }
+
+    public void deleteOddNumber() {
+        numbers.removeIf(number -> number % 2 != 0);
+        System.out.println(numbers);
+    }
+
+}
---
+++ src/Process.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class Process {
+    public static void main(String[] args) {
+        NumberExercise num = new NumberExercise();
+        Scanner sc = new Scanner(System.in);
+        int choose;
+        do {
+            System.out.println("~~~~~Menu~~~~~");
+            System.out.println("1: import Data ");
+            System.out.println("2: print Data");
+            System.out.println("3: find max number of second ");
+            System.out.println("4: delete odd number");
+            System.out.println("choose(enter 0 to exit) : ");
+
+
+            choose = sc.nextInt();
+            switch (choose) {
+                case 1:
+                    NumberExercise.importData();
+                    break;
+                case 2:
+                    num.prinData();
+                    break;
+                case 3:
+                    num.findMax2();
+                    break;
+                case 4:
+                    num.deleteOddNumber();
+                    break;
+            }
+        } while (choose != 0);
+    }
+}
